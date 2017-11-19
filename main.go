9640c4f01b41db3135c592d821eeaf98f366aa46
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-18 22:08:05 -0600

    final build

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag" // abstraction of os.Args
+	"gochat/lib"
+	"os"
+)
+
+func main() {
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "Listens on the specified ip address")
+	flag.Parse()
+
+	if isHost {
+		connIP := os.Args[2]
+		lib.RunHost(connIP)
+	} else {
+		connIP := os.Args[1]
+		lib.RunGuest(connIP)
+	}
+
+}
